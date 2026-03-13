038a98609eb3c28b1f8af3a7f62ce758e7fad189
sdziuba <sdziuba@outlook.com>
2026-03-13 16:46:18 +0100

    ssr changes

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,15 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    path: 'stocks/:exchange-ticker/peter-lynch-fair-value-calculator',
+    renderMode: RenderMode.Server, // <--- This disables Prerendering for this route
+  },
+  {
+    path: 'ru/stocks/:exchange-ticker/peter-lynch-fair-value-calculator',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: '**', // Default for all other routes
+    renderMode: RenderMode.Server,
+  },
 ];
