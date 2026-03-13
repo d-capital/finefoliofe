84737b68305918ce8356810aba03f94b1d7b8235
sdziuba <sdziuba@outlook.com>
2026-03-13 17:04:33 +0100

    fix of service for SSR

--- src/app/services/valuation.service.ts
+++ src/app/services/valuation.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { Observable, of, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
+import { Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformServer } from '@angular/common';
 
 @Injectable({
   providedIn: 'root'
@@ -9,9 +11,23 @@ import { catchError } from 'rxjs/operators';
 export class ValuationService {
   //private getValuationUrl = 'https://finefoliobe.onrender.com/valuation/'
   //private getValuationUrl = 'http://127.0.0.1:8000/valuation/'
-  private getValuationUrl = 'https://valestor.com/api/valuation/'
+  private getValuationUrl:string = "";
+  // = 'https://valestor.com/api/valuation/'
 
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) { 
+
+    if (isPlatformServer(this.platformId)) {
+      // Internal Docker address (no HTTPS needed, Caddy is skipped)
+      // Replace 'backend-container-name' with your actual backend container name
+      this.getValuationUrl = 'http://finefoliobe:8080/api/valuation'; 
+    } else {
+      // Public address for the user's browser
+      this.getValuationUrl = 'https://valestor.com/api/valuation';
+    }
+  }
 
   public getValuation(ticker:string, exchange: string): Observable<any>{
     const headers = new HttpHeaders({
